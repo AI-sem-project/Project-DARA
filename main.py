73ace50f3d5ca59f2b332f12684203088a294489
Namit Kharel <namit7425@student.ku.edu.np>
2026-08-01 19:22:18 +0545

    updated main.py for updated ridge model

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ def build_engine_instances() -> dict:
     return {
         "Simple Linear Regression": SimpleLinearRegression(),
         "Multiple Linear Regression": MultipleLinearRegression(),
-        "Ridge Regression (lambda=1.0)": RidgeRegression(lambda_=1.0),
+        "Ridge Regression (lambda=1.0)": RidgeRegression(lambda_=1.0, degree = 1),
         "Polynomial Regression (degree=2)": PolynomialRegression(degree=2),
     }
 
